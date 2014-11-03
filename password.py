ccfde53d4eb27ad05580693793f3de48d0c12edb
Daniel Jones <tortxof@gmail.com>
2014-11-03 03:45:09 -0500

    Added div container around content.

--- password.py
+++ password.py
@@ -66,7 +66,9 @@ html_template = '''\
     </div>
 </nav>
 
+<div class="container">
 {content}
+</div>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
