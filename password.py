3768b44b4e2f5e09012daa9c0df1b5439c3b87e9
Daniel Jones <tortxof@gmail.com>
2014-11-03 03:43:02 -0500

    Added nav header.

--- password.py
+++ password.py
@@ -47,6 +47,15 @@ html_template = '''\
 
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Password Manager</a>
+        </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
                 <li><a href="/">Home</a></li>
