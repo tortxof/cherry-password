a154db2de427a31746c51c39077c6020f70478b6
Daniel Jones <tortxof@gmail.com>
2014-11-12 02:01:24 -0500

    Print encrypted records for debugging.

--- export.py
+++ export.py
@@ -20,6 +20,7 @@ if bcrypt.checkpw(password, pwHash):
     print('Password is correct.')
     aes_key = toHex(bcrypt.kdf(password, salt, 16, 32))
     records = [list(i) for i in conn.execute('select * from passwords')]
+    print(records)
     for i in range(len(records)):
         records[i][3] = decrypt(aes_key, records[i][3]).decode()
         records[i][4] = decrypt(aes_key, records[i][4]).decode()
