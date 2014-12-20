17e1081ce62f135ab24262e68d500d932ef701e8
Daniel Jones <tortxof@gmail.com>
2014-12-20 18:21:48 -0500

    Added kdf test.

--- password-test.py
+++ password-test.py
@@ -13,6 +13,10 @@ class PasswordEncryption(unittest.TestCase):
         self.salt = b'\x01\xdc\xc4\xd9&\xceJ\xcb\xca\x1f\x05u\xb5r\x92H'
         self.aes_key = b'\xa9+BYT\x045\xe1\xf8\x90\xf6\xfe\xa1\xf5\x05\xd1'
 
+    def test_key_derivation(self):
+        aes_key = cherryPassword.kdf(self.password, self.salt)
+        self.assertEqual(self.aes_key, aes_key)
+
     def test_round_trip_encryption(self):
         '''Encryption followed by decryption should return back our original input.'''
         ciphertext = cherryPassword.encrypt(self.aes_key, self.plaintext)
@@ -26,11 +30,29 @@ class PasswordEncryption(unittest.TestCase):
 
     def test_bad_plaintext_input(self):
         '''Exception should be thrown when trying to encrypt anything other than str or bytes.'''
-        password = 'testpassword'
-        salt = b'\x01\xdc\xc4\xd9&\xceJ\xcb\xca\x1f\x05u\xb5r\x92H'
-        aes_key = bcrypt.kdf(password, salt, 16, 32)
         for i in (1, 0.1):
-            self.assertRaises(TypeError, cherryPassword.encrypt, aes_key, i)
+            self.assertRaises(TypeError, cherryPassword.encrypt, self.aes_key, i)
+
+class DatabaseCreation(unittest.TestCase):
+    def test_create_new_database(self):
+        pass
+
+class DatabaseAppusers(unittest.TestCase):
+    def test_add_new_appuser(self):
+        pass
+
+class DatabaseRecords(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_add_record(self):
+        pass
+
+    def test_edit_record(self):
+        pass
+
+    def test_delete_record(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
