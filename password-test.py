0be497fe83cca32fad49d0762600eb6a42aa919b
Daniel Jones <tortxof@gmail.com>
2014-12-20 16:45:13 -0500

    Save time in tests by using fixed aes_key, setUp method.

--- password-test.py
+++ password-test.py
@@ -6,25 +6,31 @@ import os
 import bcrypt
 
 class PasswordEncryption(unittest.TestCase):
+    def setUp(self):
+        self.plaintext = 'This is our test input.'
+        self.ciphertext = b'\xef\xca}\xac\xee\xd1\xd9|\xc4\x95\xee\x86\xa0\x8b\x0b\x1c^P\xf9p\x1b\xc1\x0e\xfb\xfe\x17\xd5\xc7\x8b\xfe\x99\xd9\xbeF\xf3z\x1b\xdco'
+        self.password = 'testpassword'
+        self.salt = b'\x01\xdc\xc4\xd9&\xceJ\xcb\xca\x1f\x05u\xb5r\x92H'
+        self.aes_key = b'\xa9+BYT\x045\xe1\xf8\x90\xf6\xfe\xa1\xf5\x05\xd1'
+
     def test_round_trip_encryption(self):
         '''Encryption followed by decryption should return back our original input.'''
-        original_plaintext = 'This is our test input.'
-        password = 'testpassword'
-        salt = os.urandom(16)
-        aes_key = bcrypt.kdf(password, salt, 16, 32)
-        ciphertext = cherryPassword.encrypt(aes_key, original_plaintext)
-        decrypted_plaintext = cherryPassword.decrypt(aes_key, ciphertext).decode()
-        self.assertEqual(original_plaintext, decrypted_plaintext)
+        ciphertext = cherryPassword.encrypt(self.aes_key, self.plaintext)
+        decrypted_plaintext = cherryPassword.decrypt(self.aes_key, ciphertext).decode()
+        self.assertEqual(self.plaintext, decrypted_plaintext)
 
     def test_known_ciphertext(self):
         '''Test decryption of known ciphertext.'''
-        cipher_text = b'\xef\xca}\xac\xee\xd1\xd9|\xc4\x95\xee\x86\xa0\x8b\x0b\x1c^P\xf9p\x1b\xc1\x0e\xfb\xfe\x17\xd5\xc7\x8b\xfe\x99\xd9\xbeF\xf3z\x1b\xdco'
-        plaintext = 'This is our test input.'
+        decrypted_plaintext = cherryPassword.decrypt(self.aes_key, self.ciphertext).decode()
+        self.assertEqual(self.plaintext, decrypted_plaintext)
+
+    def test_bad_plaintext_input(self):
+        '''Exception should be thrown when trying to encrypt anything other than str or bytes.'''
         password = 'testpassword'
         salt = b'\x01\xdc\xc4\xd9&\xceJ\xcb\xca\x1f\x05u\xb5r\x92H'
         aes_key = bcrypt.kdf(password, salt, 16, 32)
-        decrypted_plaintext = cherryPassword.decrypt(aes_key, cipher_text).decode()
-        self.assertEqual(plaintext, decrypted_plaintext)
+        for i in (1, 0.1):
+            self.assertRaises(TypeError, cherryPassword.encrypt, aes_key, i)
 
 if __name__ == '__main__':
     unittest.main()
